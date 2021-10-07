071c7167754aa3ac2efab4c55fab05171f12ba1e
Abu Sayed Ahmad <sayed.ahmad@technovista.com.bd>
2021-10-07 21:30:13 +0600

    Add PublisherStall

---
+++ LibraryManagementSolution/LibraryManagementSolution.Web/Migrations/LibraryManagementDB/PublisherStall/LMDB_20211007_102113_PS_PurchaseOrder.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+using System;
+
+namespace LibraryManagementSolution.Migrations.LibraryManagementDB
+{
+    [Migration(20211007_102113)]
+    public class LMDB_20211007_102113_PS_PurchaseOrder : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("PS_PurchaseOrder")
+                .WithColumn("PurchaseOrderId").AsInt64().Identity().PrimaryKey().NotNullable()
+                .WithColumn("TokenNo").AsString(50).NotNullable()
+                .WithColumn("PublisherId").AsInt64().Nullable().ForeignKey("AM_Publisher", "PublisherId")
+                .WithColumn("OrderDate").AsDateTime().Nullable()
+                .WithColumn("SubTotal").AsDecimal(8,2).Nullable()
+                .WithColumn("ServiceCharge").AsDecimal(8, 2).Nullable()
+                .WithColumn("Other").AsDecimal(8, 2).Nullable()
+                .WithColumn("InitialLess").AsDecimal(8, 2).Nullable()
+                .WithColumn("InitialPaid").AsDecimal(8, 2).Nullable()
+                .WithColumn("TotalPayable").AsDecimal(8, 2).Nullable();
+        }
+        
+    }    
+}
